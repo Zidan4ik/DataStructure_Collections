f73ec7b0690a0d2a684d09159873045371dde6aa
Zidan4ik <roman.pravnyk.tr.2021@lpnu.ua>
2023-12-14 23:26:05 +0200

    add task 1.1

--- src/interfaces_collections/MyListImp.java
+++ src/interfaces_collections/MyListImp.java
@@ -1,15 +1,16 @@
 package interfaces_collections;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class MyListImp implements MyList {
     private Integer[] array;
     private int capacity = 0;
-    private int index = -1;
+    private int index = 0;
 
 
     public MyListImp(int capacity) {
-        this.capacity=capacity;
+        this.capacity = capacity;
         array = new Integer[capacity];
         for (int i = 0; i < array.length; i++) {
             array[i] = null;
@@ -17,7 +18,7 @@ public class MyListImp implements MyList {
     }
 
     public MyListImp() {
-        this.capacity=10;
+        this.capacity = 10;
         array = new Integer[this.capacity];
         for (int i = 0; i < array.length; i++) {
             array[i] = null;
@@ -26,30 +27,53 @@ public class MyListImp implements MyList {
 
     @Override
     public int size() {
-        int count = 0;
-        for (int i = 0; i < array.length; i++) {
-            count++;
-        }
-        return count;
+        return array.length - (capacity - index);
     }
 
     @Override
     public boolean isEmpty() {
-        boolean isEmpty = true;
-        if (array.length == 0) isEmpty = true;
-        if (array.length > 0) isEmpty = false;
-        if (array.length < 0) new NegativeArraySizeException();
-        return isEmpty;
+        if (this.index > 0) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     @Override
     public boolean contains(Integer object) {
-        return false;
+        boolean flag = false;
+        for (int i = 0; i < array.length - this.index; i++) {
+            if (!array[i].equals(null)) {
+                if (array[i].equals(object)) {
+                    flag = true;
+                    break;
+                }
+            } else {
+
+            }
+        }
+        return flag;
     }
 
     @Override
     public void add(Integer object) {
-        index++;
+        int length = array.length;
+        Integer[] arrayNew;
+        if (this.index == capacity) {
+            arrayNew = new Integer[length + 5];
+            for (int i = 0; i < array.length; i++) {
+                arrayNew[i] = array[i];
+            }
+            capacity = arrayNew.length;
+            array = arrayNew;
+        }
+        array[index] = object;
+        this.index++;
+    }
+
+
+    @Override
+    public void add(int index, Integer object) {
         int length = array.length;
         Integer[] arrayNew;
         if (index == capacity) {
@@ -59,44 +83,98 @@ public class MyListImp implements MyList {
             }
             capacity = arrayNew.length;
             array = arrayNew;
+        } else if (length - (this.capacity - this.index) < index) {
+            add(object);
+        } else {
+            array[index] = object;
         }
-        array[index] = object;
-
-    }
-
-
-    @Override
-    public void add(int index, Integer object) {
 
     }
 
     @Override
     public boolean remove(Integer object) {
-        return false;
+        int indexRemove = 0;
+        boolean flag = false;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null && array[i].equals(object)) {
+                indexRemove = i;
+                flag = true;
+                break;
+            }
+        }
+        if (flag) {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (i >= indexRemove) {
+                    array[indexRemove++] = array[i + 1];
+                    array[i + 1] = null;
+                }
+            }
+        }
+        return flag;
     }
 
     @Override
     public void clear() {
-
+        this.capacity=10;
+        this.index=0;
+        this.array = new Integer[this.capacity];
     }
 
     @Override
     public Integer get(int index) {
+        for (int i = 0; i < this.array.length; i++) {
+            if(i==index){
+                return this.array[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int indexOf(Integer object) {
-        return 0;
+        int index = 0;
+        for (int i = 0; i < this.array.length; i++) {
+            if(this.array[i]==(object)){
+                index = i;
+                break;
+            }
+        }
+        return index;
     }
 
     @Override
     public int lastIndexOf(Integer object) {
-        return 0;
+        int index = 0;
+        for (int i = 0; i < this.array.length; i++) {
+            if(this.array[i]==(object)){
+                index = i;
+            }
+        }
+        return index;
     }
 
     @Override
     public String toString() {
+        Integer[] array = new Integer[this.array.length - (capacity - this.index)];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = this.array[i];
+        }
         return Arrays.toString(array);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyListImp myListImp = (MyListImp) o;
+        return capacity == myListImp.capacity && index == myListImp.index && Arrays.equals(array, myListImp.array);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(capacity, index);
+        result = 31 * result + Arrays.hashCode(array);
+        return result;
     }
 }
--- src/interfaces_collections/Test1.java
+++ src/interfaces_collections/Test1.java
@@ -1,26 +1,60 @@
 package interfaces_collections;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Test1 {
     public static void main(String[] args) {
-        MyListImp array = new MyListImp(0);
-        System.out.println(array.isEmpty());
+        MyListImp array = new MyListImp(5);
+        System.out.println("Array is empty?: " + array.isEmpty());
         array.add(2);
-//        array.add(2);
-//        array.add(2);
-//        array.add(2);
-//        array.add(2);
-//
-//        array.add(2);
-//        array.add(2);
-//        array.add(2);
-//        array.add(2);
-//        array.add(2);
-//
-//        array.add(2);
+        array.add(2);
+        array.add(2);
+        array.add(2);
+        array.add(2);
+        array.add(2);
+        array.add(null);
+        array.add(2);
         System.out.println(array);
-
         System.out.println(array.size());
+        array.add(2);
+        array.add(2);
+        array.add(2);
+        System.out.println(array);
+        System.out.println(array.size());
+        array.add(1,1);
+        array.add(12,1);
+        System.out.println(array);
+        System.out.println(array.size());
+        System.out.println(array.remove(1));
+        System.out.println(array);
+        System.out.println(array.size());
+        array.clear();
+        System.out.println(array);
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        System.out.println(array);
+        System.out.println(array.get(1));
+        array.add(1);
+        array.add(1);
+        array.add(3);
+        System.out.println();
+        System.out.println(array.indexOf(1));
+        System.out.println(array.lastIndexOf(1));
+
+        new ArrayList<>().add(1);
+        //        array.add(null);
+//        System.out.println("Array is empty?: "+array.isEmpty());
+//        System.out.println("Array: "+array);
+//        System.out.println("Size: "+array.size());
+//        System.out.println("Array contains value 5?: "+array.contains(5));
+//        System.out.println("Array contains value 3?: "+array.contains(3));
+//        System.out.println();
+//
+//        array.add(21,-2);
+//        System.out.println(array);
+
     }
 }
